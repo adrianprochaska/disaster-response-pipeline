1380e97bd64df5b260152882babe20d4b20bad94
adrianprochaska <adrian_prochaska@web.de>
2023-02-12 13:48:57 +0100

    fill process_data.py including tests

--- data/process_data.py
+++ data/process_data.py
@@ -1,15 +1,92 @@
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    """
+    Loads the messages and categories data and returns a merged dataframe.
+    """
+    # load messages dataset
+    messages = pd.read_csv(messages_filepath)
+
+    # load categories dataset
+    categories = pd.read_csv(categories_filepath)
+
+    # transform category strings into columns
+    categories = str_to_categories(categories)
+
+    # merge datasets
+    df = pd.merge(messages, categories, on='id')
+
+    return df
+
+
+def str_to_categories(categories):
+    """
+    Function transforms one column of strings into multiple columns with
+    correct category names
+    """
+    # pop index column
+    idx = categories.pop('id')
+
+    # split categories on semicolon
+    categories = categories.loc[:, 'categories'].str.split(';', expand=True)
+
+    # rename the columns of `categories`
+    categories.columns = get_category_names(categories)
+
+    # remove category name and transform string number into int
+    for column in categories.columns:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].str.get(-1)
+
+        # convert column from string to numeric
+        categories[column] = categories[column].astype(int)
+
+    # merge index and new categories dataframe
+    categories = pd.concat([idx, categories], axis=1, join='inner')
+
+    return categories
+
+
+def get_category_names(categories):
+    """
+    output
+    """
+    # select the first row of the categories dataframe
+    row = categories.iloc[0, :]
+
+    # use this row to extract a list of new column names for categories.
+    # one way is to apply a lambda function that takes everything
+    # up to the second to last character of each string with slicing
+    category_colnames = row.str.split('-', expand=True).iloc[:, 0].to_list()
+    return category_colnames
 
 
 def clean_data(df):
-    pass
+    """
+    This function cleans the data.
+    First, it removes rows with invalid data.
+    Second, it drops duplicates.
+    """
+    # find rows with invalid data in the category columns
+    # values not equal to zero or one are invalid
+    idx_cell_invalid = (df.iloc[:, 4:] != 1) & (df.iloc[:, 4:] != 0)
+    idx_row_invalid = idx_cell_invalid.any(axis=1)
+
+    # drop invalid rows
+    df = df.loc[~idx_row_invalid, :]
+
+    # drop duplicates
+    df = df.drop_duplicates()
+
+    return df
 
 
 def save_data(df, database_filename):
+    engine = create_engine('sqlite:///{}'.format(database_filename))
+    df.to_sql('InsertTableName', engine, index=False, if_exists='replace')
     pass
 
 
--- tests.py
+++ tests.py
@@ -1,8 +1,94 @@
-class TestClass:
-    def test_pep8_data(self):
-        assert 1 == 2
-        pass
+import os
+import data.process_data as process_data
+import pandas as pd
 
-    def test_pep8_data2(self):
-        assert 1 == 1
-        pass
+
+class TestClass:
+    """
+    Test class for disaster response pipeline.
+    Tests the basic scripts of this project
+    """
+
+    def test_process_data(self):
+        """
+        Runs the script process_data.py with the given arguments.
+        """
+        os.system(('process_data.py disaster_messages.csv '
+                   'disaster_categories.csv DisasterResponse.db'))
+
+    def test_load_data(self):
+        """
+        Tests the load_data function
+        """
+        process_data.load_data(
+            'data\\disaster_messages.csv',
+            'data\\disaster_categories.csv'
+        )
+
+    def test_str_to_categories(self):
+        """
+        Tests the str_to_categories function
+        """
+        # test input
+        dict_test = {
+            'id': [1, 2],
+            'categories': [
+                'related-1;request-1;offer-0',
+                'related-1;request-2;offer-1'
+            ]
+        }
+        df_test = pd.DataFrame(dict_test)
+
+        # validation dataframe
+        dict_true = {
+            'id': [1, 2],
+            'related': [1, 1],
+            'request': [1, 2],
+            'offer': [0, 1]
+        }
+        df_true = pd.DataFrame(dict_true)
+
+        categories = process_data.str_to_categories(df_test)
+
+        pd.testing.assert_frame_equal(categories, df_true, check_dtype=False)
+
+        assert True
+
+    def test_get_category_names(self):
+        """
+        Tests the get_category_names function
+        """
+        dict_test = {
+            '1': ['related-1', 'related-0'],
+            '2': ['request-0', 'request-0'],
+            '3': ['offer-0', 'offer-1']
+        }
+
+        df_test = pd.DataFrame(dict_test)
+        category_names = process_data.get_category_names(df_test)
+        assert category_names == ['related', 'request', 'offer']
+
+    def test_clean_data(self):
+        """
+        Test the clean_data function
+        """
+        # test input
+        dict_test = {
+            'id': [1, 2, 3, 3],
+            '1': 4*[None],
+            '2': 4*[None],
+            '3': 4*[None],
+            'related': [1, 1, 1, 1],
+            'request': [1, 2, 0, 0],
+            'offer': [0, 1, 1, 1]
+        }
+        df_test = pd.DataFrame(dict_test)
+
+        # validation dataframe
+        df_true = df_test.iloc[[0, 2], :]
+
+        df_cleaned = process_data.clean_data(df_test)
+
+        pd.testing.assert_frame_equal(df_cleaned, df_true, check_dtype=False)
+
+        assert True
